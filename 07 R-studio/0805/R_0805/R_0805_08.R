11435ab1672924399b1e14bb297e6beaeab7468f
hune102377 <hune102377@gmail.com>
2022-08-05 18:07:54 +0900

    0805_06

---
+++ 07 R-studio/0805/R_0805/R_0805_08.R
@@ -0,0 +1,72 @@
+# 8. 데이터 전처리 (2)
+# 선택, 집계, 분리, 결합, 정렬
+
+
+# subset() : 필요한 데이터를 추출
+st <- data.frame(state.x77)
+
+# 인구가 가장 많은 도시의 3번째와 6번째 칼럼을 넣기
+st[st$Population == max(st$Population), c(3,6)]
+
+# 이를 subset으로 하기
+subset(st,
+       subset = st$Population == max(st$Population),
+       select = c(3,6))
+
+set <-iris[iris$Species == 'setosa',]
+vrs <-iris[iris$Species == 'versicolor',]
+vir <-iris[iris$Species == 'virginica',]
+
+sp <- split(iris, f = iris$Species)
+length(sp)
+names(sp)
+class(sp)
+
+sp$setosa
+sp$versicolor
+sp$virginica
+
+dim(sp$setosa)
+
+df.2 <- rbind(sp$setosa, sp$versicolor)
+dim(df.2)
+
+# merge() 함수
+
+library(readxl)
+df.1 <- read_excel("성적표.xlsx", sheet =1)
+df.2 <- read_excel("성적표.xlsx", sheet =2)
+df.1
+df.2
+
+df <- merge(df.1, df.2, all=T,
+      by.x = c('번호','이름'),
+      by.y = c('No', '이름')
+)
+
+df
+
+df$`Deep Learning`
+
+str(df)
+
+colnames(df) <- c('no',
+                  'name',
+                  'python',
+                  'r',
+                  'ml',
+                  'dl',
+                  'cloud'
+                  )
+
+# aggregate 함수
+df <- iris
+# 품종으로 구분되어 1~4번째 평균의 집합
+aggregate(df[, -5],
+          by = list(품종 = df$Species),
+          FUN = mean)
+
+# 품종으로 구분되어 1~4번째 표준편차차의 집합
+aggregate(df[, -5],
+          by = list(품종 = df$Species),
+          FUN = sd)
